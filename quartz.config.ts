651034054a6d7d6f82f7abdc7c10655732194070
AZapie <azaier03@gmail.com>
2024-10-14 14:09:41 -0500

    woops, meant to change secondary color in dark mode

--- quartz.config.ts
+++ quartz.config.ts
@@ -1,4 +1,4 @@
-import { QuartzConfig } from "./quartz/cfg"
+import type { QuartzConfig } from "./quartz/cfg"
 import * as Plugin from "./quartz/plugins"
 
 /**
@@ -45,8 +45,8 @@ const config: QuartzConfig = {
           gray: "#646464",
           darkgray: "#d4d4d4",
           dark: "#ebebec",
-          secondary: "#7b97aa",
-          tertiary: "#e74856",
+          secondary: "#e74856",
+          tertiary: "#84a59d",
           highlight: "rgba(143, 159, 169, 0.15)",
           textHighlight: "#b3aa0288",
         },
